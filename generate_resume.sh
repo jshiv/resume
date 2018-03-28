a517972a1dc1f94bdfc93deeb3b68942060ebbdf
Jason Shiverick <jshiv00@gmail.com>
2018-03-28 14:53:42 -0700

    added .txt generation and file using pandoc

--- generate_resume.sh
+++ generate_resume.sh
@@ -15,5 +15,9 @@ docker exec -it resume xvfb-run md2resume pdf -t unstyled README.md pdf/
 docker stop resume && docker rm resume
 mv pdf/README.pdf pdf/resume.pdf
 
+
+# Generate zip file using pandoc
+pandoc --from=markdown --to=plain --output=txt/resume.txt README.md
+
 #copy the file to the md folder
 cp README.md md/resume.md
