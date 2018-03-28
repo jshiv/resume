e3883e0b119b1fc4aeb451f48d19946f8395ce7d
Jason Shiverick <jshiv00@gmail.com>
2018-03-28 13:45:15 -0700

    added docker resume generator for pdf

--- generate_resume.sh
+++ generate_resume.sh
@@ -8,9 +8,12 @@ md2resume README.md
 mv README.html html/resume.html
 
 # Generate PDF file
-md2resume --pdf README.md
-mv README.pdf pdf/resume.pdf
-
+#md2resume --pdf README.md
+#mv README.pdf pdf/resume.pdf
+docker run -dt --name resume -v ${PWD}:/resume -w /resume there4/markdown-resume bash
+docker exec -it resume xvfb-run md2resume pdf -t unstyled README.md pdf/
+docker stop resume && docker rm resume
+mv pdf/README.pdf pdf/resume.pdf
 
 #copy the file to the md folder
 cp README.md md/resume.md
