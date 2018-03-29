e3a85c132616442973852249bdb7f01b222f02d6
Jason Shiverick <jshiv00@gmail.com>
2018-03-28 17:33:07 -0700

    added pandoc pdf resume

--- generate_resume.sh
+++ generate_resume.sh
@@ -1,6 +1,8 @@
-#brew install #npm
-
-# For usage on the command line
+## Install
+#brew install npm
+#brew cask install mactex
+#ln -s /Library/TeX/Root/bin/x86_64-darwin/pdflatex /usr/local/bin/pdflatex
+#conda install pandoc
 #npm install -g markdown-resume
 
 # Generate HTML file
@@ -15,6 +17,9 @@ docker exec -it resume xvfb-run md2resume pdf -t unstyled README.md pdf/
 docker stop resume && docker rm resume
 mv pdf/README.pdf pdf/resume.pdf
 
+
+#Generate PDF using pandoc
+pandoc -o pdf/resume_latex.pdf README.md
 
 # Generate txt file using pandoc
 pandoc --from=markdown --to=plain --output=txt/resume.txt README.md
