7ceff05654412cd989131f4b5130ddc17b3240fb
Jason Shiverick <jshiv00@gmail.com>
2018-10-30 13:38:39 -0700

    updated generate_resume processes to be docker dependent

--- generate_resume.sh
+++ generate_resume.sh
@@ -1,30 +1,24 @@
-## Install
-#brew install npm
-#brew cask install mactex
-#ln -s /Library/TeX/Root/bin/x86_64-darwin/pdflatex /usr/local/bin/pdflatex
-#conda install pandoc
-#npm install -g markdown-resume
+## Install docker
 
-# Generate HTML file
-md2resume README.md
-mv README.html html/resume.html
+# build the dependencies
+docker build -t local/markdown-resume ./build/
 
 # Generate PDF file
-#md2resume --pdf README.md
-#mv README.pdf pdf/resume.pdf
-docker run -v ${PWD}:/resume there4/markdown-resume:2.3.1 md2resume pdf -t unstyled README.md pdf/
-docker stop resume && docker rm resume
+docker run -v ${PWD}:/resume local/markdown-resume md2resume pdf -t unstyled README.md pdf/
 mv pdf/README.pdf pdf/resume.pdf
 
+# Generate HTML file
+docker run -v ${PWD}:/resume local/markdown-resume md2resume html README.md html/
+mv html/README.html html/resume.html
 
-#Generate PDF using pandoc
-pandoc -o pdf/resume_latex.pdf README.md
+#Generate latex PDF using pandoc
+docker run -v ${PWD}:/resume local/markdown-resume pandoc -o pdf/resume_latex.pdf README.md
 
 # Generate txt file using pandoc
-pandoc --from=markdown --to=plain --output=txt/resume.txt README.md
+docker run -v ${PWD}:/resume local/markdown-resume pandoc --from=markdown --to=plain --output=txt/resume.txt README.md
 
 # Generate docx file using pandoc
-pandoc --from=markdown --to=docx --output=docx/resume.docx README.md
+docker run -v ${PWD}:/resume local/markdown-resume pandoc --from=markdown --to=docx --output=docx/resume.docx README.md
 
 #copy the file to the md folder
 cp README.md md/resume.md
